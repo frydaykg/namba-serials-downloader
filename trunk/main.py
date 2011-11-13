5a2ded7b8b2db156611caf83639e78bc163014b7
fryday.kg@gmail.com <fryday.kg@gmail.com@cdff0466-fcc0-ad4d-fbfa-0efacca801dc>
2011-11-13 08:38:25 +0000

    Add finding of difference

--- trunk/main.py
+++ trunk/main.py
@@ -14,8 +14,41 @@ def GetUnavailableSeasons(seasons,avSeasons):
 		if intersectSeasons!=seasons:
 			return sorted(list(set(seasons) ^ set(intersectSeasons)))
 
+def GetRequested(seasons,episodes):
+	result={}
+	if not seasons:
+		result['all']={}
+	elif not episodes:
+		for i in seasons:
+			result[i]={'all':''}
+	else:
+		result[seasons[0]]={}
+		for i in episodes:
+			result[seasons[0]][i]=''
+	return result
 
 
+def GetProcAndDiff(req,av):
+	notAv={}
+	proc={}
+	for i in req.keys():
+		if i=='all':
+			proc=av
+		elif i not in av.keys():
+			notAv[i]=req[i]
+		else:
+			for j in req[i]:
+				if j=='all':
+					proc[i]=av[i]
+				elif j not in av[i].keys():
+					if i not in notAv.keys():
+						notAv[i]={}
+					notAv[i][j]=req[i][j]
+				else:
+					if i not in proc.keys():
+						proc[i]={}
+					proc[i][j]=av[i][j]
+	return (proc,notAv)
 
 def main():
 	argv=sys.argv[1:]
@@ -26,26 +59,29 @@ def main():
 	seasons=sorted(GetUniqueList(GetSeasonsFromArgv(argv)))
 	episodes=sorted(GetUniqueList(GetEpisodesFromArgv(argv)))
 	av=ParseAvailableSeasonsAndEpisodes(pageData)
+	req=GetRequested(seasons,episodes)
 	
+	(proc,notAv)=GetProcAndDiff(req,av)
+	
+	if notAv:
+		if len(notAv)>1:			
+			print 'Seasons ' + str(notAv.keys()).replace('\'','').replace(']','').replace('[','') + ' unvailable. Continue? (y/n)'
+		elif 'all' in notAv[notAv.keys()[0]].keys():
+			print 'Season ' + str(notAv.keys()).replace('\'','').replace(']','').replace('[','') + ' unvailable. Continue? (y/n)'
+		elif len(notAv[notAv.keys()[0]])==1:
+			print 'Episode ' + str(notAv[notAv.keys()[0]].keys()).replace('\'','').replace(']','').replace('[','') + ' unvailable. Continue? (y/n)'
+		else:
+			print 'Episodes ' + str(notAv[notAv.keys()[0]].keys()).replace('\'','').replace(']','').replace('[','') + ' unvailable. Continue? (y/n)'
+		answer=raw_input()
+		if answer!='y':
+			exit()
+		else:
+			temp={}
+			for i in seasons:
+				if i in av.keys():						
+					temp[i]=av[i]
+			seasons=temp
 
-	if not seasons:
-		pass
-	else:
-		unavSeasons=GetUnavailableSeasons(seasons,av.keys())
-		if unavSeasons:
-			print 'Seasons ' + str(unavSeasons).replace('\'','').replace(']','').replace('[','') + ' unvailable. Continue? (y/n)'
-			answer=raw_input()
-			if answer!='y':
-				exit()
-			else:
-				temp={}
-				for i in seasons:
-					if i in av.keys():						
-						temp[i]=av[i]
-				seasons=temp
-
-
-	print 'Seasons ' + str(seasons.keys()).replace('\'','').replace(']','').replace('[','') + ' will be proceed!'
 	print 'Finish!'
 
 
